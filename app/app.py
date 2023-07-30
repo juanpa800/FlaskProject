a7f896851726ca9b5ee3ac3de5827afdfab6692b
canojuanpa800 <canojuanpa@gmail.com>
2023-07-29 20:09:26 -0500

    add index.html in templates folder

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola mundo! desde debug mode"
+    # return "Hola mundo! desde debug mode"
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)#, port=500)
