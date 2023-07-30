2cca301258faab3c94888dd657cdf68f1483d7f8
canojuanpa800 <canojuanpa@gmail.com>
2023-07-29 20:14:40 -0500

    use a dic for dinamic template

--- app/app.py
+++ app/app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # return "Hola mundo! desde debug mode"
-    return render_template('index.html')
+    data = {
+        'tab_title':'Titulo de la pagina web',
+        'title': 'Este es el titulo de mi pagina web'
+    }
+    # data es un diccionario con parametros para usar la plantilla de forma dinámica
+    return render_template('index.html', data)
 
 if __name__=='__main__':
     app.run(debug=True)#, port=500)
